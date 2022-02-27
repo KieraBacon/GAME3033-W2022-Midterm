2aa36090f6b94802b2327e66a398f15a84216906
Kiera Bacon <KieraJBacon@Outlook.com>
2022-02-27 07:33:24 -0500

    Added orbit to planets.

--- Assets/[Scripts]/Planet.cs
+++ Assets/[Scripts]/Planet.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -10,5 +11,55 @@ public class Planet : MonoBehaviour
     [SerializeField]
     private SphereCollider _atmosphereCollider;
     public SphereCollider atmosphereCollider => _atmosphereCollider;
+    
+    [Header("Orbit")]
+    [SerializeField]
+    private float orbitRadius;
+    [SerializeField]
+    private float orbitSpeed;
+    [SerializeField]
+    private float orbitStartingAngle;
+    private float orbitAngle;
 
+    [Header("Rotation")]
+    [SerializeField]
+    private Vector3 rotationAxis;
+    [SerializeField]
+    private float rotationSpeed;
+    [SerializeField]
+    private float rotationStartingAngle;
+    private float rotationAngle;
+
+    private void OnValidate()
+    {
+        SetToOrbitPosition(orbitStartingAngle);
+        SetToRotation(rotationStartingAngle);
+    }
+
+    private void Start()
+    {
+        orbitAngle = orbitStartingAngle;
+    }
+
+    private void FixedUpdate()
+    {
+        orbitAngle += Time.fixedDeltaTime * orbitSpeed;
+        SetToOrbitPosition(orbitAngle);
+
+        rotationAngle += Time.fixedDeltaTime * rotationSpeed;
+        SetToRotation(rotationAngle);
+
+    }
+
+    private void SetToOrbitPosition(float angle)
+    {
+        transform.localPosition = Vector3.zero;
+        transform.RotateAround(transform.position + Vector3.right * orbitRadius, Vector3.up, angle);
+        transform.localPosition = transform.localPosition - Vector3.right * orbitRadius;
+    }
+
+    private void SetToRotation(float angle)
+    {
+        transform.rotation = Quaternion.AngleAxis(angle, rotationAxis);
+    }
 }
